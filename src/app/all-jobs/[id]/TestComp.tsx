8d58dd67880c97230cb6f1f90fcb742d6c403411
Xaid-vfx <mohd.zaid.123786@gmail.com>
2024-04-08 02:40:39 +0530

    test

--- src/app/all-jobs/[id]/TestComp.tsx
+++ src/app/all-jobs/[id]/TestComp.tsx
@@ -1,5 +1,11 @@
 'use client'
+
+import { useSearchParams } from "next/navigation";
+
 export default function TestComp(props) {
+    const search = useSearchParams()
+    console.log(search.get('id'));
+
     console.log(props.pr);
     console.log(props.job);
     return (
