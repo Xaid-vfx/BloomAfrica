4e0e001b61fb1198fbd374ca990ec4e65c81079e
Mohammad Zaid <zaid@Mohammads-MacBook-Air.local>
2024-05-26 17:42:00 +0530

    Mobile view filters fixed

---
+++ src/app/all-jobs/MobileViewJobs.tsx
@@ -0,0 +1,111 @@
+'use client'
+import MobileCard from "@/components/Jobs/MobileCard/MobileCard"
+import ShowFilters from "./ShowFilters"
+import GetStarted from "@/components/GetStartedBanner/GetStarted"
+import { useEffect, useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { IoCloseSharp, IoFilter } from "react-icons/io5";
+import FilterSidebar from "@/components/Jobs/FilterSidebar/FilterSidebar";
+
+async function getJobs() {
+    const supabase = createClientComponentClient()
+    const { data, error } = await supabase
+        .from('Jobs')
+        .select()
+
+    if (error) {
+        console.log(error);
+    }
+
+    return data;
+}
+
+export default function MobileViewJobs(props) {
+    const [jobs, setjobs] = useState()
+    const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+    const arr = ["Software", "Electronics", "Design"]
+    const [showfilter, setshowfilter] = useState(false)
+
+
+    const handleCategoryChange = (category) => {
+        if (selectedCategories.includes(category)) {
+            setSelectedCategories(selectedCategories.filter(cat => cat !== category));
+        } else {
+            setSelectedCategories([...selectedCategories, category]);
+        }
+        console.log(selectedCategories);
+    };
+
+    const handleTypeChange = (type) => {
+        if (selectedTypes.includes(type)) {
+            setSelectedTypes(selectedTypes.filter(t => t !== type));
+        } else {
+            setSelectedTypes([...selectedTypes, type]);
+        }
+        console.log(selectedTypes);
+    };
+
+    useEffect(() => {
+        getJobs().then((data) => {
+            const renderJobs = data?.filter(job => {
+                // Check if the title includes the search query (case-insensitive)
+                const titleMatch = job.title.toLowerCase().includes(props.search?.toLowerCase());
+
+                // Check if the location includes the location query (case-insensitive)
+                const locationMatch = job.location.toLowerCase().includes(props.location?.toLowerCase());
+                const categoryMatch = selectedCategories.length === 0 || selectedCategories.includes(job.category);
+
+                const typeMatch = selectedTypes.length === 0 || selectedTypes.includes(job.type);
+
+                // Return true if both conditions are met
+                return titleMatch && locationMatch && categoryMatch && typeMatch;
+            });
+            setjobs(renderJobs)
+        })
+    }, [props.location, props.search, selectedCategories, selectedTypes])
+
+
+    return (
+        <div>
+            <div className="py-10 flex flex-col w-full px-4 lg:hidden">
+                <div className="flex justify-between w-full items-center">
+                    <h1 className="text-xl">All Jobs</h1>
+                    {/* <p className="text-sm">Most relevant</p> */}
+                </div>
+                <div className="flex items-center justify-between">
+                    <p className="text-left my-2 font-light text-sm text-[#7C8493]">Showing {props.renderJobs?.length} results</p>
+                    {showfilter ? <div className="h-[200vh] m-0 w-full left-0 top-0 z-10 fixed bg-white overflow-scroll">
+                        <div onClick={() => { setshowfilter(false) }} className="flex justify-center items-center text-center pt-6 pb-10 text-base cursor-pointer text-red-700"><IoCloseSharp className="text-2xl" /><div>Close</div></div>
+                        <FilterSidebar selectedCategories={selectedCategories} selectedTypes={selectedTypes} handleCategoryChange={handleCategoryChange} handleTypeChange={handleTypeChange} />
+                    </div> : <p onClick={() => {
+                        setshowfilter(true)
+                    }} className="text-sm flex items-center gap-1 bg-slate-100 px-2 py-1 rounded-lg cursor-pointer"><IoFilter className="text-xl" />Filters</p>}
+                </div>
+
+
+
+                {/* Cards */}
+
+                <div className="flex flex-col gap-4 lg:hidden my-6">
+                    {jobs?.map((job: JobProps) => {
+                        return <MobileCard
+                            id={job.uid}
+                            logo={job.companylogo}
+                            title={job.title}
+                            category={job.category}
+                            location={job.location}
+                            salary={job.salary}
+                            type={job.type}
+                            description={job.description}
+                            extras={job.extras}
+                            responsibilities={job.responsibilities}
+                            who_you_are={job.who_you_are}
+                        />
+                    })}
+                </div>
+            </div>
+            <div className="px-4 lg:hidden"><GetStarted /></div>
+        </div>
+    )
+}
--- src/components/Jobs/FilterSidebar/FilterSidebar.tsx
+++ src/components/Jobs/FilterSidebar/FilterSidebar.tsx
@@ -10,12 +10,12 @@ export default function FilterSidebar(props: any) {
                 <p className="font-semibold">Types of Employement</p>
                 <div className="text-[#515B6F] flex flex-col gap-4">
                     <div className='flex'>
-                        <input type='checkbox' value="Full Time" onChange={(e) => {
+                        <input type='checkbox' checked={props.selectedTypes?.includes('Full Time')} value="Full Time" onChange={(e) => {
                             props.handleTypeChange("Full Time")
                         }} /><p className="ml-3">Full Time</p>
                     </div>
                     <div className='flex'>
-                        <input type='checkbox' value="Part Time" onChange={(e) => {
+                        <input type='checkbox' checked={props.selectedTypes?.includes('Part Time')} value="Part Time" onChange={(e) => {
                             props.handleTypeChange("Part Time")
                         }} /><p className="ml-3">Part Time</p>
                     </div>
@@ -24,41 +24,46 @@ export default function FilterSidebar(props: any) {
                 <p className="font-semibold mt-8">Categories</p>
                 <div className="text-[#515B6F] flex flex-col gap-4">
                     <div className='flex'>
-                        <input type='checkbox' value={'Electrical Engineering'} onChange={() => props.handleCategoryChange('Electrical Engineering')}
+                        <input type='checkbox' checked={props.selectedCategories?.includes('Technology')} value="Technology" onChange={(e) => {
+                            props.handleCategoryChange("Technology")
+                        }} /><p className="ml-3">Technology </p>
+                    </div>
+                    <div className='flex'>
+                        <input type='checkbox' checked={props.selectedCategories?.includes('Electrical Engineering')} value={'Electrical Engineering'} onChange={() => props.handleCategoryChange('Electrical Engineering')}
                         /><p className="ml-3">Electrical Engineering </p>
                     </div>
                     <div className='flex'>
-                        <input type='checkbox' value="Mechanical Engineering" onChange={(e) => {
+                        <input type='checkbox' checked={props.selectedCategories?.includes('Mechanical Engineering')} value="Mechanical Engineering" onChange={(e) => {
                             props.handleCategoryChange("Mechanical Engineering")
                         }} /><p className="ml-3">Mechanical Engineering </p>
                     </div>
                     <div className='flex'>
-                        <input type='checkbox' value="Construction & Civil Engineering" onChange={(e) => {
+                        <input type='checkbox' checked={props.selectedCategories?.includes('Construction & Civil Engineering')} value="Construction & Civil Engineering" onChange={(e) => {
                             props.handleCategoryChange("Construction & Civil Engineering")
                         }} /><p className="ml-3">Construction & Civil Engineering </p>
                     </div>
                     <div className='flex'>
-                        <input type='checkbox' value="Business Entreprenuership" onChange={(e) => {
+                        <input type='checkbox' checked={props.selectedCategories?.includes('Business Entreprenuership')} value="Business Entreprenuership" onChange={(e) => {
                             props.handleCategoryChange("Business Entreprenuership")
                         }} /><p className="ml-3">Business Entreprenuership</p>
                     </div>
                     <div className='flex'>
-                        <input type='checkbox' value="Cosmetology" onChange={(e) => {
+                        <input type='checkbox' checked={props.selectedCategories?.includes('Cosmetology')} value="Cosmetology" onChange={(e) => {
                             props.handleCategoryChange("Cosmetology")
                         }} /><p className="ml-3">Cosmetology </p>
                     </div>
                     <div className='flex'>
-                        <input type='checkbox' value="Hospitality" onChange={(e) => {
+                        <input type='checkbox' checked={props.selectedCategories?.includes('Hospitality')} value="Hospitality" onChange={(e) => {
                             props.handleCategoryChange("Hospitality")
                         }} /><p className="ml-3">Hospitality </p>
                     </div>
                     <div className='flex'>
-                        <input type='checkbox' value="Fashion" onChange={(e) => {
+                        <input type='checkbox' checked={props.selectedCategories?.includes('Fashion')} value="Fashion" onChange={(e) => {
                             props.handleCategoryChange("Fashion")
                         }} /><p className="ml-3">Fashion </p>
                     </div>
                     <div className='flex'>
-                        <input type='checkbox' value="Food & Cullinary" onChange={(e) => {
+                        <input type='checkbox' checked={props.selectedCategories?.includes('Food & Cullinary')} value="Food & Cullinary" onChange={(e) => {
                             props.handleCategoryChange("Food & Cullinary")
                         }} /><p className="ml-3">Food & Cullinary </p>
                     </div>
