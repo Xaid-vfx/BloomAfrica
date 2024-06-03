3ffde17d94a3436b40d6849f98d80afc2510edeb
Mohammad Zaid <zaid@Mohammads-MacBook-Air.local>
2024-06-03 18:14:00 +0530

    UI fix

--- src/app/all-jobs/Search.tsx
+++ src/app/all-jobs/Search.tsx
@@ -31,7 +31,7 @@ export default function Search() {
     }
 
     return (
-        <div className="bg-white p-4 mx-10 w-[80%] lg:w-auto lg:mx-0 rounded-2xl lg:flex items-center box_shadow x lg:p-2 lg:pl-8 lg:rounded-3xl">
+        <div className="bg-white p-4 w-[90%] lg:w-auto lg:mx-0 rounded-2xl lg:flex items-center box_shadow x lg:p-2 lg:pl-8 lg:rounded-3xl">
             <div className="flex gap-1 lg:items-center">
                 <CiSearch className="text-3xl" />
                 <input onKeyDown={(e) => { e.key == "Enter" ? handleSearch() : "" }} onChange={(e) => { setquery(e.target.value) }} type="text" className="border-b-[1px] lg:mx-2 px-4 pt-2 pb-1  placeholder:text-sm mb-4 w-full text-sm  lg:shadow-none lg:mb-0 lg:rounded-r-none lg:placeholder:font-light placeholder:text-[#7C8493] lg:placeholder:text-sm lg:w-auto outline-none" placeholder="Job title or keywords" />
