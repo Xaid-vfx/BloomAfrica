035a6be30858002fb246e6dc75fb826826c617e6
Xaid-vfx <mohd.zaid.123786@gmail.com>
2024-07-08 01:55:44 +0530

    Initiate chat from listings/Send a message modal

--- src/components/Recruiter/Listing/Listing.tsx
+++ src/components/Recruiter/Listing/Listing.tsx
@@ -11,6 +11,8 @@ import { IoMdArrowRoundBack } from "react-icons/io";
 import ApplicantDisplay from "./ApplicantDisplay";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { IoLocationOutline } from "react-icons/io5";
+import { Dialog } from "@radix-ui/react-dialog";
+import { DialogDemo } from "@/components/Modal/Modal";
 
 
 type Props = {
@@ -23,11 +25,13 @@ export default function Listing(props: Props) {
 
     const [showJobApplications, setshowJobApplications] = useState(false)
     const [showApplicantDetails, setshowApplicantDetails] = useState(false)
+    const [seekerChatId, setseekerChatId] = useState(false)
     const [loading, setloading] = useState(false)
     const [applications, setapplications] = useState([])
     const [applicant, setapplicant] = useState()
     const [experience, setexperience] = useState()
     const router = useRouter()
+
 
     async function deleteJob(id: string) {
         console.log(id);
@@ -97,7 +101,6 @@ export default function Listing(props: Props) {
     return (
         <div className="lg:px-8 lg:py-8 w-full h-full lg:bg-[#F5F5F5] overflow-scroll">
             <div className="">
-
                 {showJobApplications ?
                     <div>
                         {
@@ -136,7 +139,7 @@ export default function Listing(props: Props) {
                                         <h1 className="font-semibold text-2xl pb-4 pl-8">Applications</h1>
                                         {
                                             loading ? <div className="flex justify-center items-center h-[300px]">
-                                                <MoonLoader color="#4A2C84" /> </div> : applications?.length > 0 ? <StickyHeadTable fetchApplicantDetails={fetchApplicantDetails} applications={applications} /> : <div className="flex justify-center items-center h-[200px]">
+                                                <MoonLoader color="#4A2C84" /> </div> : applications?.length > 0 ? <StickyHeadTable user={props.user} seekerChatId={setseekerChatId} fetchApplicantDetails={fetchApplicantDetails} applications={applications} /> : <div className="flex justify-center items-center h-[200px]">
                                                     No applications found!
                                                 </div>
                                         }
