ddffcc841a2921d3f50482a62de92d2f87fa0942
Xaid-vfx <mohd.zaid.123786@gmail.com>
2024-08-15 03:45:47 +0530

    Agreement button disable

--- src/components/Modal/AgreementModal.tsx
+++ src/components/Modal/AgreementModal.tsx
@@ -50,7 +50,7 @@ export function AgreementModal(props) {
                     <input onChange={(e) => { setAgreementCheckbox(e.target.checked) }} type="checkbox" />
                     <p className="text-xs md:text-sm">I agree to the following terms and conditions</p>
                 </div>
-                <Button onClick={props.handleAgreement} className={`bg-[#4A2C84] px-16 w-full md:w-auto ${agreementCheckbox ? "" : "cursor-not-allowed"}`} type="submit">
+                <Button disabled={!agreementCheckbox} onClick={props.handleAgreement} className={`bg-[#4A2C84] px-16 w-full md:w-auto ${agreementCheckbox ? "" : "cursor-not-allowed"}`} type="submit">
                     Post
                 </Button>
             </DialogContent>
