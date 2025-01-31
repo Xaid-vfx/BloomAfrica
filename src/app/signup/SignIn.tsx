245384c252041ced89e3ae8711382243da370504
Xaid-vfx <mohd.zaid.123786@gmail.com>
2025-01-31 19:37:01 +0530

    replace url

--- src/app/signup/SignIn.tsx
+++ src/app/signup/SignIn.tsx
@@ -79,7 +79,7 @@ export default function SignIn() {
                     access_type: 'offline',
                     prompt: 'consent'
                 },
-                redirectTo: signUpUserTypeTab == "seeker" ? 'https:/bloom.africa/auth/callback?route=/signup/complete_profile&next=' + redirectUrl : 'https:/bloom.africa/auth/callback?route=/signup/complete_recruiter_profile'
+                redirectTo: signUpUserTypeTab == "seeker" ? 'https://www.bloom.africa/auth/callback?route=/signup/complete_profile&next=' + redirectUrl : 'https://www.bloom.africa/auth/callback?route=/signup/complete_recruiter_profile'
             },
 
         })
