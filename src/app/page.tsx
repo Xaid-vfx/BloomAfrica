7a94e68f8db000b9eef5a51148a6d596ad47a617
Xaid-vfx <mohd.zaid.123786@gmail.com>
2025-02-11 19:53:40 +0530

    add browser signin redirect on landing - fix server/client issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-'use client'
-
-import { useEffect } from 'react'
 import Footer from '@/components/Footer/Footer'
 import Navbar from '../components/navbar/Navbar'
 import Hero from '@/components/Landing/Hero/Hero'
@@ -16,36 +13,16 @@ import ExploreCategory from '@/components/Landing/ExploreCategory'
 import ForSeekersRecruiters from '@/components/Landing/ForSeekersRecruiters'
 import Testimonials from '@/components/Landing/Testimonials'
 import Banner from '@/components/Landing/Banner'
+import FacebookBrowserRedirect from '@/components/FacebookBrowserRedirect'
 
 export const metadata: Metadata = {
   title: 'Bloom'
 }
 
 export default function Home() {
-  useEffect(() => {
-    // Check if user is in Facebook's in-app browser
-    const isFacebookBrowser = /FB_IAB|FBAN|FBAV/.test(navigator.userAgent);
-
-    if (isFacebookBrowser) {
-      const currentURL = window.location.href;
-
-      // For iOS
-      if (/iPhone|iPad|iPod/.test(navigator.userAgent)) {
-        window.location.href = `x-web-search://?${currentURL}`;
-        // Fallback to Safari if x-web-search doesn't work
-        setTimeout(() => {
-          window.location.href = currentURL;
-        }, 500);
-      }
-      // For Android
-      else {
-        window.location.href = `intent://${window.location.host}${window.location.pathname}${window.location.search}#Intent;scheme=https;package=com.android.chrome;end`;
-      }
-    }
-  }, []);
-
   return (
     <main className="flex flex-col ">
+      <FacebookBrowserRedirect />
       <div className='hidden lg:block gradient'>
         <Navbar />
         <Hero />
