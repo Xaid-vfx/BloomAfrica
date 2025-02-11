bdb964c14a10902d46f0efa05c55ec096a34f613
Xaid-vfx <mohd.zaid.123786@gmail.com>
2025-02-11 19:49:07 +0530

    add browser sigin redirect on landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useEffect } from 'react'
 import Footer from '@/components/Footer/Footer'
 import Navbar from '../components/navbar/Navbar'
 import Hero from '@/components/Landing/Hero/Hero'
@@ -19,6 +22,28 @@ export const metadata: Metadata = {
 }
 
 export default function Home() {
+  useEffect(() => {
+    // Check if user is in Facebook's in-app browser
+    const isFacebookBrowser = /FB_IAB|FBAN|FBAV/.test(navigator.userAgent);
+
+    if (isFacebookBrowser) {
+      const currentURL = window.location.href;
+
+      // For iOS
+      if (/iPhone|iPad|iPod/.test(navigator.userAgent)) {
+        window.location.href = `x-web-search://?${currentURL}`;
+        // Fallback to Safari if x-web-search doesn't work
+        setTimeout(() => {
+          window.location.href = currentURL;
+        }, 500);
+      }
+      // For Android
+      else {
+        window.location.href = `intent://${window.location.host}${window.location.pathname}${window.location.search}#Intent;scheme=https;package=com.android.chrome;end`;
+      }
+    }
+  }, []);
+
   return (
     <main className="flex flex-col ">
       <div className='hidden lg:block gradient'>
