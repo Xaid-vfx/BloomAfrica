fa82a2c0e7d925ec67d6726f736cd1866102ee20
DavidIO <davidonadipe101@gmail.com>
2026-01-04 00:02:16 +0000

    footer update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import PrentisLogoWhite from "../Logo/PrentisLogoWhite";
 
 export default function Footer() {
   return (
-    <div className="bg-[#171c1f] lg:bg-[#1D1B1B]">
+    <div className="bg-[#0A1F44]">
       <div className=" px-8 py-10 text-white lg:flex lg:justify-between lg:px-24 ms-auto me-auto max-w-[1750px] ">
         <div className="lg:w-1/3">
           <div className="flex items-center gap-2">
@@ -14,28 +14,28 @@ export default function Footer() {
           <p className="text-xs leading-6 mt-2 lg:text-sm lg:mt-4 lg:leading-7">Join Prentis and become part of a community of builders shaping Africa's tech future through real product experience and expert mentorship.</p>
 
           <div className="socialicons mt-5 mb-8 flex gap-4 lg:mt-6">
-            <a target="blank" href="https://x.com/_bloomAfrica" className="bg-black p-3 rounded-full cursor-pointer"><FaXTwitter /></a>
-            <a target="blank" href="https://www.linkedin.com/company/bloomafrica1" className="bg-black p-3 rounded-full cursor-pointer"><FaLinkedinIn /></a>
-            <a target="blank" href="https://facebook.com/PrentisApprenticeships1" className="bg-black p-3 rounded-full cursor-pointer"><FaFacebook /></a>
+            <a target="blank" href="https://x.com/_bloomAfrica" className="bg-white/10 p-3 rounded-full cursor-pointer hover:bg-[#14B8A6] transition-all"><FaXTwitter /></a>
+            <a target="blank" href="https://www.linkedin.com/company/bloomafrica1" className="bg-white/10 p-3 rounded-full cursor-pointer hover:bg-[#14B8A6] transition-all"><FaLinkedinIn /></a>
+            <a target="blank" href="https://facebook.com/PrentisApprenticeships1" className="bg-white/10 p-3 rounded-full cursor-pointer hover:bg-[#14B8A6] transition-all"><FaFacebook /></a>
           </div>
         </div>
 
         <div className="lg:flex lg:w-1/2 lg:justify-evenly">
           <div className="flex flex-col">
             <h2 className="text-lg font-medium lg:text-xl lg:font-medium">Company</h2>
-            <a href="/" className="text-sm font-light my-3">Home</a>
-            {/* <a href="/about" className="text-sm font-light my-3">About</a> */}
+            <a href="/" className="text-sm font-light my-3 hover:text-[#14B8A6] transition-colors cursor-pointer">Home</a>
+            {/* <a href="/about" className="text-sm font-light my-3 hover:text-[#14B8A6] transition-colors cursor-pointer">About</a> */}
           </div>
 
           <div className="mt-12 lg:mt-0 flex flex-col">
             <h2 className="text-lg font-medium lg:text-xl lg:font-medium">Legal</h2>
-            <a href="/privacy-policy" className="text-sm font-light my-3">Privacy Policy</a>
-            <a href="/terms-of-service" className="text-sm font-light my-3">Terms of Use</a>
+            <a href="/privacy-policy" className="text-sm font-light my-3 hover:text-[#14B8A6] transition-colors cursor-pointer">Privacy Policy</a>
+            <a href="/terms-of-service" className="text-sm font-light my-3 hover:text-[#14B8A6] transition-colors cursor-pointer">Terms of Use</a>
           </div>
         </div>
       </div>
-      <hr className="border-[#505050]" />
-      <div className="py-6 bg-[#1D1B1B] text-center text-white font-light">Prentis 2025. All Rights Reserved</div>
+      <hr className="border-white/20" />
+      <div className="py-6 bg-[#0A1F44] text-center text-white font-light">Prentis 2025. All Rights Reserved</div>
     </div>
   )
 }
